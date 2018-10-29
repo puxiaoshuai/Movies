5867ac8093cf6bf0c64ffaccfd51cabedea93b20
puxiaoshuai <1372553910@qq.com>
2018-10-29 18:05:04 +0800

    使用bootstrap4.布局练习

--- manage.py
+++ manage.py
@@ -1,16 +1,14 @@
 from flask import render_template
+from datetime import timedelta
+
 
 from app import app_task
 
-from flask_bootstrap import Bootstrap
-
-Bootstrap(app_task)
-
-
+app_task.config['TEMPLATES_AUTO_RELOAD'] = True
 @app_task.errorhandler(404)
 def page_not_found(e):
     return render_template('error_page/error404.html'), 404
 
 
 if __name__ == '__main__':
-    app_task.run()
+    app_task.run(port=1000,debug=True)
