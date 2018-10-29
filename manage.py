2877c1290823b943cd7385a42d813fadeb56e688
puxiaoshuai <1372553910@qq.com>
2018-10-29 23:14:53 +0800

    修改主题

--- manage.py
+++ manage.py
@@ -1,14 +1,15 @@
 from flask import render_template
 from datetime import timedelta
 
-
 from app import app_task
 
 app_task.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
 @app_task.errorhandler(404)
 def page_not_found(e):
     return render_template('error_page/error404.html'), 404
 
 
 if __name__ == '__main__':
-    app_task.run(port=1000,debug=True)
+    app_task.run(port=1000, debug=True)
